06067bfbe16b61fd47d05af70842650dda268f0d
paulinek13 <paulinek13@outlook.com>
2023-10-07 16:42:29 +0200

    refactor: simplify the date range comparison to a single return statement

--- utils.py
+++ utils.py
@@ -13,10 +13,11 @@ class CriticalError(Exception):
 
 
 def is_date_within_range(date: str, start_date: str, end_date: str) -> bool:
-    _date = datetime.strptime(date, "%Y-%m-%d")
-    _start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    _end_date = datetime.strptime(end_date, "%Y-%m-%d")
-    return _start_date <= _date <= _end_date
+    return (
+        datetime.strptime(start_date, "%Y-%m-%d")
+        <= datetime.strptime(date, "%Y-%m-%d")
+        <= datetime.strptime(end_date, "%Y-%m-%d")
+    )
 
 
 TODAY: str = datetime.now().strftime("%Y-%m-%d")
