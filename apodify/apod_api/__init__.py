369ecacdf0ba87384eb87376efc95754c0b21308
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-04-08 18:24:43 +0200

    feat: add module for NASA's APOD API service

---
+++ apodify/apod_api/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module provides interfaces to interact with NASA's Astronomy Picture of the Day (APOD) service.
+Includes clients and data handling logic for API communication.
+"""
+
+from apodify.apod_api.apod_client import APODClient
+
+__all__ = [
+    "APODClient",
+]
