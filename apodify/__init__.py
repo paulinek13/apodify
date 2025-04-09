588ebe4c8427938c144d602b04888245609a497e
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-04-09 15:18:57 +0200

    feat: configure `loguru` for custom logging

--- apodify/__init__.py
+++ apodify/__init__.py
@@ -1,2 +1,13 @@
 __name__ = "apodify"
 __version__ = "0.1.0"
+
+if __name__ == "__main__" or __name__ == "apodify":
+    from sys import stdout
+    from loguru import logger
+
+    logger.remove()
+    logger.add(
+        stdout,
+        colorize=True,
+        format="<c>{time:YY-MM-DD.HH:mm:ss}</c> <level>{level: <8}</level> <level>{message}</level>",
+    )
